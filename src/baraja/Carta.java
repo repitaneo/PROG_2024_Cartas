bd23a03da04080dc6d715c316f00bd5623aa419a
Marcos <Marcos@Profesor>
2024-01-22 08:51:23 +0100

    Clase Carta creada
    - valor
    - palo
    - setters y getters
    - toString

--- src/baraja/Carta.java
+++ src/baraja/Carta.java
@@ -2,4 +2,28 @@ package baraja;
 
 public class Carta {
 
+	private char valor;
+	private char palo;
+	
+	
+	public char getValor() {
+		return valor;
+	}
+	public void setValor(char valor) {
+		this.valor = valor;
+	}
+	public char getPalo() {
+		return palo;
+	}
+	public void setPalo(char palo) {
+		this.palo = palo;
+	}
+	
+	@Override
+	public String toString() {
+		return "[" + valor + "" + palo + "]";
+	}
+	
+	
+	
 }
